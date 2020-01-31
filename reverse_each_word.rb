8e525b45601a56fcf7233f05a04e00bf7fdf7d69
Samantha Meason <sbmeason@gmail.com>
2020-01-31 03:04:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,12 @@
-def reverse_each_word(sentence)
-  sentence.split
-  new_sentance = sentence.split
-  new_sentance.map do |word|
-      word.reverse
+def reverse_each_word(array)
+  rev_words = []
+  new_array = "#{array}".split(" ")
+  new_array.collect do |word|
+    rev_words << word.reverse
+  end
+  rev_words.join(" ")
 end
+
 
 
 
